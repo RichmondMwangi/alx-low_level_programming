903f1a9838e83e6238b5be936f678cbe5318f65e
Richmond Mwangi <gathiiyamwangi@gmail.com.com>
2023-02-17 16:37:57 -0800

    When i can't sleep, i play the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- Entry point
+ * Description: a program that prints the alphabet in lowercase
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putcha('n\');
+	return (0);
+}
+
