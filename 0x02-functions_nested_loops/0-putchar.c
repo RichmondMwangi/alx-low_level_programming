ee944acc6f6faa2783e47f301ad1ba8ebf7bac87
Richmond Mwangi <gathiiyamwangi@gmail.com.com>
2023-02-21 02:00:35 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * Description -  a program that prints _putchar
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	_putchar("_putchar,\n");
+	return (0);
+}
