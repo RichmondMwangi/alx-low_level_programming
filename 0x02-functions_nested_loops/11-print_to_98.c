793af6b3de6514c5c23e3117ad02b2174f0e0833
Richmond Mwangi <gathiiyamwangi@gmail.com.com>
2023-02-22 03:22:37 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,33 @@
 */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n >= 0 && n <= 98)
+	if (n < 98)
 	{
-		for (i = n ; i <= 98 ; i++)
+		while (n <= 98)
 		{
-			printf("%d, ", i);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
-		printf("\n");
-	}
-	else if (n < 0)
-	{
-		for (i = n ; i <= 98 ; i++)
-		{
-			printf("%d, ", i);
-		}
-		printf("\n");
 	}
 	else if (n > 98)
 	{
-		for (i = n ; i >= 98 ; i--)
+		while (n >= 98)
 		{
-			printf("%d, ", i);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
 		}
-		printf("\n");
 	}
-	else if (n == 98)
-		printf("%d, ", n);
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
 }
